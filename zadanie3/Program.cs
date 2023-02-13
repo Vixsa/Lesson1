b8bd0da3030f41742c8e94a00077ead6b9bdbd2b
Vixsa <victoriagiter@gmail.com>
2023-02-13 20:44:02 +0200

    Третье задание

---
+++ zadanie3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число = ");
+int x = int.Parse(Console.ReadLine());
+int y = x;
+x = -x; 
+while (x<=y)
+    {if ((x % 2) == 0)
+    Console.Write(x + " ," );
+    x++;
+    }
