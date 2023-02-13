64b1aeb9fe64db26f5b1bb8a5680edf134153bf1
Vixsa <victoriagiter@gmail.com>
2023-02-13 19:57:29 +0200

    Первая задача

---
+++ zadanie1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число №1 =");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число №2 =");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число №3 =");
+int c = int.Parse(Console.ReadLine());
+int max = 1;
+if (a>max) max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+Console.WriteLine ("Максимум = " + max);
